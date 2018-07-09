ad12639f87dc16fde16de45127128caf7379af06
Riley Durant <rileyduraant@gmail.com>
2018-07-09 00:19:37 -0400

    [FEAT]: New PrivateView implementation

--- app/components/PrivateView.js
+++ app/components/PrivateView.js
@@ -1,65 +1,11 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
-import { biometrics } from '../helpers';
+const PrivateView = ({ children, isUnlocked }) =>
+  isUnlocked ? children : null;
 
-import { VibrancyView } from 'react-native-blur';
-import AppStateUpdate from './AppStateUpdate';
-
-class PrivateView extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isUnlocked: false,
-    };
-
-    // this.handleAppStateChange = this.handleAppStateChange.bind(this);
-    // this.authenticateBiometrics = this.authenticateBiometrics.bind(this);
-  }
-
-  componentDidMount() {
-    // this.authenticateBiometrics();
-  }
-
-  /*
-  authenticateBiometrics() {
-    biometrics.authenticate(isUnlocked => {
-      this.setState({
-        isUnlocked: isUnlocked,
-      });
-    });
-  }
-  */
-
-  /*
-  handleAppStateChange(state) {
-    console.log('bbbbbb', state);
-
-    if (state === 'inactive') {
-      this.setState({
-        isUnlocked: false,
-      });
-    
-  }
-  */
-
-  render() {
-    return (
-      <VibrancyView
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-        }}
-        blurType="dark"
-      >
-        <Text>{this.state.isUnlocked ? 'Unlocked' : 'Locked'}</Text>
-      </VibrancyView>
-    );
-  }
-}
+PrivateView.propTypes = {
+  isUnlocked: PropTypes.bool.isRequired,
+  children: PropTypes.element.isRequired,
+};
 
 export default PrivateView;
--- app/screens/BookmarkViewer.js
+++ app/screens/BookmarkViewer.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
-import BookmarkViewerHeader from '../components/BookmarkViewer/Header';
+// import BookmarkViewerHeader from '../components/BookmarkViewer/Header';
 import PrivateView from '../components/PrivateView';
+
+import { biometrics } from '../helpers';
 
 export default class BookmarkViewer extends Component {
   constructor(props) {
     super(props);
 
     console.log('BookmarkViewer props', props);
+
+    this.state = {
+      isUnlocked: true,
+    };
   }
+
+  componentDidMount() {
+    /*
+    biometrics.authenticate(unlocked => {
+      this.setState({
+        isUnlocked: unlocked,
+      });
+    });
+    */
+  }
+
   render() {
     const { item } = this.props;
-    return <PrivateView />;
-
-    /*
+    const { isUnlocked } = this.state;
     return (
-      <View style={styles.container}>
-        <BookmarkViewerHeader
-          url={item.url}
-          image={'https://i.imgur.com/sdO8tAw.png'}
-        />
-      </View>
+      <PrivateView isUnlocked={isUnlocked}>
+        <Text>
+          {isUnlocked} lol {item.title}
+        </Text>
+      </PrivateView>
     );
-    */
   }
 }
 
