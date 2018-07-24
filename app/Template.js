85648f2d0301542b563a6de41a98fc9cf661f71b
Riley Durant <rileyduraant@gmail.com>
2018-07-24 09:50:57 -0400

    [FEAT]: Added react-native-vector-icons
    [FEAT]: Updated router stuff with tab bar info

--- app/Template.js
+++ app/Template.js
@@ -1,49 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
 
-import Bookmarks from './screens/Bookmarks';
-import BookmarkViewer from './screens/BookmarkViewer';
-import Settings from './screens/Settings';
+import RootStack from './router';
 
 import store from './store';
-
-class HomeScreen extends React.Component {
-  componentDidMount() {
-    console.log('okay!');
-  }
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
-}
-
-const BookmarkStack = createStackNavigator({
-  Bookmark: {
-    screen: Bookmarks,
-  },
-  BookmarkViewer: {
-    screen: BookmarkViewer,
-  },
-});
-
-const SettingsStack = createStackNavigator({
-  Settings: {
-    screen: Settings,
-  },
-});
-
-const RootStack = createBottomTabNavigator({
-  Bookmark: BookmarkStack,
-  Settings: SettingsStack,
-});
 
 const Template = () => (
   <Provider store={store}>
---
+++ app/router.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Bookmarks from './screens/Bookmarks';
+import BookmarkViewer from './screens/BookmarkViewer';
+import Settings from './screens/Settings';
+
+const BookmarkStack = createStackNavigator({
+  Bookmark: {
+    screen: Bookmarks,
+  },
+  BookmarkViewer: {
+    screen: BookmarkViewer,
+  },
+});
+
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: Settings,
+  },
+});
+
+const RootStack = createBottomTabNavigator(
+  {
+    Bookmark: BookmarkStack,
+    Settings: SettingsStack,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        // We only have two tabs, Settings and Bookmarks
+        // So we can just use a ternary operator for now
+        const iconName =
+          routeName === 'Bookmark'
+            ? `ios-bookmarks${focused ? '' : '-outline'}`
+            : `ios-options${focused ? '' : '-outline'}`;
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+export default RootStack;
