2fc7b659648c47f435d14d6a194f8bda665ac0ff
Riley Durant <rileyduraant@gmail.com>
2018-08-13 17:28:40 -0400

    [FEAT]: Began working on custom form inputs

--- app/components/Forms/AddBookmarkForm/Input.js
+++ app/components/Forms/AddBookmarkForm/Input.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
 import styled from 'styled-components';
 
+const Container = styled.View`
+  padding: 5px;
+`;
+
+const Title = styled.Text`
+  color: white;
+`;
+
 const TextInput = styled.TextInput`
-  background-color: white;
+  height: 25px;
+  color: white;
+  border-bottom-color: white;
+  border-bottom-width: 1px;
 `;
 
 const Input = props => (
-  <TextInput
-    onChangeText={props.onChangeText}
-    name={props.name}
-    value={props.value}
-  />
+  <Container>
+    <Title>{props.name.toUpperCase()}</Title>
+    <TextInput
+      onChangeText={props.onChangeText}
+      name={props.name}
+      value={props.value}
+    />
+  </Container>
 );
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+};
 
 export default Input;
